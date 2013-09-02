d45858189e38218d056740ca4ddbbdbfbfe43f02
Jon Cram <jon@webignition.net>
2013-09-02 14:17:39 +0100

    Make deploy more explicit regarding remote branches

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-git checkout gh-pages && git merge master && git checkout master && git push
+git checkout gh-pages && git merge master && git checkout master && git push origin master && git push origin gh-pages
