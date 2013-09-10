af97ad0bdc50a46c69244fe0982b6530bb298bb4
Jon Cram <jon@webignition.net>
2013-09-10 11:59:38 +0100

    Correctly remove incorrectly-encoded &id in href, fix double-encoded ampersands

--- _build/fix-html-validation-issues.js
+++ _build/fix-html-validation-issues.js
@@ -1,31 +1,43 @@
 var fs = require('fs');
 var $ = require('jquery');
 
-var posts_directory = __dirname + '/../_posts';
+var replace_incorrectly_encoded_href_id = function (content, match_result) {
+    var new_href = match_result[0].replace('&id=', '&amp;id=');
+    return content.substr(0, match_result.index) + new_href + content.substr(match_result.index + match_result[0].length);
+};
 
+var replace_double_encoded_ampersand = function (content, match_result) {
+    var new_href = match_result[0].replace('&amp;amp;', '&amp;');    
+    return content.substr(0, match_result.index) + new_href + content.substr(match_result.index + match_result[0].length);
+};
+
+var posts_directory = __dirname + '/../_posts';
 var posts = fs.readdirSync(posts_directory);
 
-for (var post_index = 0; post_index < posts.length; post_index++) {    
+for (var post_index = 0; post_index < posts.length; post_index++) {        
     var post_path = posts_directory + '/' + posts[post_index];        
     var content = fs.readFileSync(post_path, "utf8");
     
     var content_sections = content.split("---\n\n");
-    
-    var jqueryified = $('<div/>').html(content_sections[1]);
-    
     var is_modified = false;
     
-    $('a', jqueryified).each(function () {
-        var anchor = $(this);
-        if (anchor.attr('href').indexOf('&id=') !== -1) {
-            console.log('Fixing unencoded ampersand in query string for "'+posts[post_index]+'"');
-            anchor.attr('href', anchor.attr('href').replace('&id=', '&amp;id='));
-            is_modified = true;
-        }
-    });
+    var incorrectly_encoded_href_id_match_result = content_sections[1].match(/href=".*&id=.*"/);
+    while (incorrectly_encoded_href_id_match_result !== null) {
+        console.log('Fixing unencoded ampersand in query string for "'+posts[post_index]+'"');
+        content_sections[1] = replace_incorrectly_encoded_href_id(content_sections[1], incorrectly_encoded_href_id_match_result);        
+        is_modified = true;
+        incorrectly_encoded_href_id_match_result = content_sections[1].match(/href=".*&id=.*"/);
+    }
+    
+    var double_encoded_ampersand_match_result = content_sections[1].match(/href=".*&amp;amp;.*"/);
+    while (double_encoded_ampersand_match_result !== null) {
+        console.log('Fixing double-encoded ampersand in query string for "'+posts[post_index]+'"');
+        content_sections[1] = replace_double_encoded_ampersand(content_sections[1], double_encoded_ampersand_match_result);        
+        is_modified = true;        
+        double_encoded_ampersand_match_result = content_sections[1].match(/href=".*&amp;amp;.*"/);
+    }
     
     if (is_modified) {
-        content_sections[1] = jqueryified.html();
         fs.writeFileSync(post_path, content_sections.join("---\n\n"), "utf8", function (err) {        
             console.log(err);
             process.exit();
